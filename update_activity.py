270a122f832798c6a1d138484eb0ce4836bd4922
odpy <odpy@odpy>
2024-01-02 22:11:58 +0800

    update activity action

--- update_activity.py
+++ update_activity.py
@@ -9,14 +9,19 @@ try:
     for i in activity_table["basicInfo"]:
         if i.endswith("side") or i.endswith("sre"):
             startTime = activity_table["basicInfo"][i]["startTime"]
-            activity_start_time_list.append(startTime)
+            endTime = activity_table["basicInfo"][i]["endTime"]
+            activity_start_time_list.append((startTime, endTime))
 
-    max_activity_start_time = max(activity_start_time_list)
+    max_activity_start_time, max_activity_end_time = max(
+        activity_start_time_list, key=lambda k: k[0]
+    )
 
     with open("config/config.json") as f:
         config = json.load(f)
 
     config["userConfig"]["activityMinStartTs"] = max_activity_start_time - \
+        (7*24*60*60)
+    config["userConfig"]["activityMaxStartTs"] = max_activity_end_time + \
         (7*24*60*60)
 
     with open("config/config.json", "w") as f:
