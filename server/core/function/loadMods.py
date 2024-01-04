ca87179645a3277afb7ca34529bc6366309a734e
odpy <odpy@odpy>
2024-01-04 10:07:22 +0800

    cache loadMods

--- server/core/function/loadMods.py
+++ server/core/function/loadMods.py
@@ -4,6 +4,7 @@ import zipfile
 import hashlib
 from datetime import datetime
 import zlib
+import json
 
 
 def writeLog(data):
@@ -26,6 +27,35 @@ def loadMods(log: bool = True):
     for file in os.listdir('./mods/'):
         if file != ".placeholder" and file.endswith(".dat"):
             fileList.append('./mods/' + file)
+
+    dat_file_infos = {}
+
+    for filePath in fileList:
+        with open(filePath, "rb") as f:
+            file_content = f.read()
+            file_size = len(file_content)
+            file_crc32 = zlib.crc32(file_content)
+            dat_file_infos[filePath] = {
+                "size": file_size,
+                "crc32": file_crc32
+            }
+
+    mod_cache = None
+
+    if os.path.isfile("mods.json"):
+        with open("mods.json", encoding="utf-8") as f:
+            mod_cache = json.load(f)
+
+    mod_cache_valid = False
+
+    if mod_cache is not None:
+        cached_dat_file_infos = mod_cache["file"]
+        if dat_file_infos == cached_dat_file_infos:
+            mod_cache_valid = True
+
+    if mod_cache_valid:
+        writeLog(filePath + ' - \033[1;32mUsing Cached Mod...\033[0;0m')
+        return mod_cache["mod"]
 
     for filePath in fileList:
         if not zipfile.is_zipfile(filePath) or os.path.getsize(filePath) == 0:
@@ -65,5 +95,14 @@ def loadMods(log: bool = True):
         except:
             writeLog(filePath + ' - \033[1;31mMod file loading failed...\033[0;0m')
         modFile.close()
+
+    with open("mods.json", "w", encoding="utf-8") as f:
+        json.dump(
+            {
+                "file": dat_file_infos,
+                "mod": loadedModList
+            }, f,
+            sort_keys=False, indent=4
+        )
 
     return loadedModList
